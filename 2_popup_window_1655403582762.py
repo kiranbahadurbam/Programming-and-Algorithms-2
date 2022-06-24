f63067e4557c3fb3aad055a7d747f22064f16af7
kiranbahadurbam <91513491+kiranbahadurbam@users.noreply.github.com>
2022-06-23 19:15:09 -0700

    Add files via upload

---
+++ 2_popup_window_1655403582762.py
@@ -0,0 +1,4 @@
+
+import PySimpleGUI as sg
+
+sg.popup('Hello from Nepal','This is the shortest GUI program.')
---
+++ import PySimpleGUI.py
@@ -0,0 +1,6 @@
+import PySimpleGUI  as sg 
+print(sg)
+print(sg.version)
+
+import PySimpleGUI as sg
+sg.popup('Hello from pySimpleGUI!','This is the shortest GUI program.')
